9a1eb2a0684c468d4739eb7ede5fe91960b1a6ef
Igor Durovic <xen0phile@bitter-anarchist.bitter-anarchist.com>
2015-07-15 15:51:34 -0600

    fixed request form, javascript and php connected, app.yaml updated for changes

--- site/js/contact_me.js
+++ site/js/contact_me.js
@@ -10,7 +10,7 @@ $(function() {
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
+            var date = $("input#date").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
@@ -18,11 +18,11 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "././request/send_request.php",
                 type: "POST",
                 data: {
                     name: name,
-                    phone: phone,
+                    date: date,
                     email: email,
                     message: message
                 },
