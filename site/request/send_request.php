c3a15fec67ed2416df34ff846985905481a2ad38
IgorDurovic <id.tesla@gmail.com>
2015-08-16 21:59:39 -0600

    password and email verification, valid email parser added

--- site/request/send_request.php
+++ site/request/send_request.php
@@ -1,21 +1,56 @@
 <?php
+
+class Entry{
+	public $teacher = "";
+	public $start = -1;
+	public $end = -1;
+	public $duration = -1;
+}
+
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
    empty($_POST['date']) 		||
-   empty($_POST['message'])	||
+   empty($_POST['start'])		||
+   empty($_POST['password']))	||
+   empty()
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
 	echo "No arguments Provided!";
 	return false;
    }
+
+$password = $_POST['start'];
+if($password == "panther"){
+	echo "Incorrect Password";
+	return false;
+}
+
+$curFileR = fopen("schedule.txt", "r") or die("Unable to open file!");
+$curFileW = fopen("schedule.txt", "w") or die("Unable to open file!");
+
+
 	
 $name = $_POST['name'];
 $email_address = $_POST['email'];
+
+$validEmails = fopen("valid-mails.txt", "r") or die("Unable to open file");
+
+while(true){
+	$temp = fget($validEmails);
+	if(temp == $email_address){
+		break;
+	}
+	else if(feof($validEmails)){
+		echo "Invalid email";
+		return false;
+	}
+}
+
 $date = $_POST['date'];
 $message = $_POST['message'];
-
 $to = 'id.tesla@gmail.com';
+
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\ndate: $date\n\nMessage:\n$message";
 $headers = "From: $email_address\n";
