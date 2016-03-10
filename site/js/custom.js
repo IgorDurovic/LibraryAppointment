05f3a4d0a27f2e8429ee9d0c2ae753bb6437eb4f
lolcatslol <christopher.chen10@gmail.com>
2016-03-10 08:55:24 -0700

    Added files via upload

--- site/js/custom.js
+++ site/js/custom.js
@@ -1,7 +1,43 @@
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-function display_day_information( month, day ) {
-    var elem = document.getElementById( "day-information" );
-    console.log( month );
-    elem.innerHTML = "<p>" + months[ month ] + ", " + day + "</p>";
+function display_day_information( month, day, year ) {
+	
+	
+	
 }
+
+function readTextFile( file )
+{
+	alert('reading file');
+	/*
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+*/
+
+    var reader = new FileReader();
+	reader.onload = function(event) {
+		var contents = event.target.result;
+		console.log("File contents: " + contents);
+	};
+
+reader.onerror = function(event) {
+	console.error("File could not be read! Code " + event.target.error.code);
+};
+
+	reader.readAsText(file);
+}
+/**/
+
